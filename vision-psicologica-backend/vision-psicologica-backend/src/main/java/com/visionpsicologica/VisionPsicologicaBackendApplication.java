12ebb7fd561d3d89ec29c797a560db95cbfec3e8
FelipeVillota10 <david.villota@correounivalle.edu.co>
2026-05-25 19:12:42 -0500

    Se añadió la carpeta donde estará el backend del proyecto

---
+++ vision-psicologica-backend/vision-psicologica-backend/src/main/java/com/visionpsicologica/VisionPsicologicaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.visionpsicologica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionPsicologicaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionPsicologicaBackendApplication.class, args);
+	}
+
+}
---
+++ vision-psicologica-backend/vision-psicologica-backend/src/test/java/com/visionpsicologica/VisionPsicologicaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visionpsicologica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionPsicologicaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
