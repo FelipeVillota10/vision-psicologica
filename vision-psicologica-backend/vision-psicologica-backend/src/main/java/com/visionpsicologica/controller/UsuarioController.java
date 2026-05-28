9b4ae2f57e328c3ffbe5b6d0381ffa674cd28917
FelipeVillota10 <david.villota@correounivalle.edu.co>
2026-05-27 20:04:50 -0500

    Ahora los usuarios pueden iniciar sesion

--- vision-psicologica-backend/vision-psicologica-backend/src/main/java/com/visionpsicologica/controller/UsuarioController.java
+++ vision-psicologica-backend/vision-psicologica-backend/src/main/java/com/visionpsicologica/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -67,5 +68,17 @@ public class UsuarioController {
                     return ResponseEntity.ok(usuario);
                 })
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUsuario(@RequestBody Map<String, String> credentials) {
+        String email = credentials.get("email");
+        String contrasena = credentials.get("contrasena");
+        boolean esValido = usuarioService.autenticar(email, contrasena);
+        if (esValido) {
+            return ResponseEntity.ok().body(Map.of("message", "Autenticación exitosa"));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Email o contraseña incorrectos"));
+        }
     }
 }
